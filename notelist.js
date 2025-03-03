d03bd0eeea0ea2da5f4569155107acede7702dfd
hunain shahid <hunainshahid124@gmail.com>
2025-03-03 09:08:04 -0800

    note list

--- notelist.js
+++ notelist.js
@@ -1,7 +1,25 @@
+let signup = document.querySelector(".signup");
+let homecontainner = document.querySelector(".homecontainner"); // Added missing dot (.)
 let email = document.querySelector("#email");
 let password = document.querySelector("#password");
 
-function login_form(){
-    if(!email.value || !password.value) return alert("Please add email & password");
+function login_form() {
+    if (!email.value || !password.value) {
+        return alert("Please add email & password");
+    }
     localStorage.setItem("email", email.value);
+    checkIsUserLogin();
 }
+
+function checkIsUserLogin() {
+    let storedEmail = localStorage.getItem("email"); // Fixed key name
+    if (storedEmail) {
+        signup.style.display = "none";
+        homecontainner.style.display = "block";
+    } else {
+        signup.style.display = "block";
+        homecontainner.style.display = "none";
+    }
+}
+
+checkIsUserLogin();
